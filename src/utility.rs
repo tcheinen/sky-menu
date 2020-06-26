648e9847cde75382ad0f51607449f7d1214c13dc
Teddy Heinen <teddy@heinen.dev>
2020-06-25 22:20:41 -0500

    put together utility function to get windows from i3 ipc

--- src/utility.rs
+++ src/utility.rs
@@ -1,3 +1,5 @@
+use i3ipc::reply::Node;
+use i3ipc::I3Connection;
 use std::env;
 use std::path::PathBuf;
 
@@ -15,4 +17,25 @@ pub fn get_xdg_application_dirs() -> impl Iterator<Item = PathBuf> {
         .into_iter()
         .map(|x| PathBuf::from(x).join("applications"))
         .filter(|x| x.exists())
+}
+
+fn workspace(node: &Node) -> Vec<Node> {
+    if node.window.is_some() {
+        return vec![node.clone()];
+    }
+    node.nodes.iter().flat_map(workspace).collect::<Vec<Node>>()
+}
+
+pub fn get_running_applications() {
+    let mut conn = I3Connection::connect().expect("I3 IPC connection failed");
+    let root = conn.get_tree().expect("couldn't get I3 tree");
+    root.nodes[1]
+        .nodes
+        .iter()
+        .find(|x| x.name == Some("content".into()))
+        .unwrap()
+        .nodes
+        .iter()
+        .flat_map(workspace)
+        .for_each(|x| println!("{:#?}", x));
 }
